b9d146ed622651ff558f54f312b1372d99709a03
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-09-14 19:10:58 +0900

    Modify Error DB_backup

--- db_backup.sh
+++ db_backup.sh
@@ -1,8 +1,8 @@
 echo "Backup start!"
 
 current=$(date +"%Y-%m-%d_%H:%M:%S")
-
-docker exec mysql /usr/bin/mysqldump -u oic_yju --password=oic_yju e_global_zone > DB_backup/backup_$current.sql
+dir="/home/ubuntu"
+docker exec mysql /usr/bin/mysqldump -u oic_yju --password=oic_yju e_global_zone > $dir/E_Global_Zone/server/DB_backup/backup_$current.sql
 
 echo "Backup complete. Check the path 저장경로"
 
