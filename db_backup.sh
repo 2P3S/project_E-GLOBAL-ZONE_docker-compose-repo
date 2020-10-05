2ed6bf51a1406e7a7d9e684b17e997b02217034c
root <root@ip-172-31-47-5.ap-northeast-2.compute.internal>
2020-10-06 01:18:59 +0900

    [modify] db-backup, nginx-default setting

--- db_backup.sh
+++ db_backup.sh
@@ -1,8 +1,14 @@
 echo "Backup start!"
 
 current=$(date +"%Y-%m-%d_%H:%M:%S")
+today=$(date +"%Y년 %m월 %d일")
+
+backup_command="cat backup_$current.sql | docker exec -i mysql /usr/bin/mysql -u oic_yju --password=oic_yju e_global_zone"
+
 dir="/home/ubuntu"
 docker exec mysql /usr/bin/mysqldump -u oic_yju --password=oic_yju e_global_zone > $dir/E_Global_Zone/server/DB_backup/backup_$current.sql
 
-echo "Backup complete. Check the path 저장경로"
+echo "[ $today 기준 존 백업 Data ]\n\n백업 명령어 : $backup_command" | mutt -s "[BACK UP_ZONE] $today" oic.yju@gmail.com -a $dir/E_Global_Zone/server/DB_backup/backup_$current.sql
+
+echo "[$current] Backup complete. Check the path 저장경로"
 
